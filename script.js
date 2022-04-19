8c659f72a53d0e5add9796bc15cd708f672695a0
matt8094 <github@azerox.net>
2022-04-18 19:06:27 -0500

    Add function for button fade in

--- script.js
+++ script.js
@@ -95,6 +95,7 @@ function playRound(user, choiceID){
     
     displayResult(roundPoint, gameText);
     addScore(roundPoint);
+    setTimeout(function(){buttonReturn();}, 1500);
 };
 // change buttons class to animate
 function buttonAnimation(targetButtonID){
@@ -107,4 +108,19 @@ function buttonAnimation(targetButtonID){
         }
     });
     
+};
+
+function buttonReturn() {
+    // Sets the buttons to fade back in and then put them on standby
+    // for the next round
+    document.getElementById('s').className='fade-in';
+    document.getElementById('r').className='fade-in';
+    document.getElementById('p').className='fade-in';
+    setTimeout(function(){buttonStandby();}, 200);
+};
+
+function buttonStandby() {
+    document.getElementById('s').className='standby';
+    document.getElementById('r').className='standby';
+    document.getElementById('p').className='standby';
 };
